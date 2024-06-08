d716590a36098f88e9ec357e530f3b3bccf0ecc9
sebastian <sebastianipl0131@gmail.com>
2024-06-08 16:19:12 -0400

    inicio de navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ import Header from './components/Header';
 function App() {
   return (
     <>
-    <Header />
-      <h1>Sebastian Marrera</h1>
-      <h2 style={{color: 'var(--secondary)'}}>React Developer | Game Developer</h2>
+      <Header />
     </>
   )
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,24 +1,15 @@
 import './Header.css'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
   return (
-    <Navbar expand="lg" className="header">
-      <Container>
-        <Navbar.Brand href="#home">Sebastian Marrera</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-          <Nav className="me-auto">
-            <Nav.Link className='navLink' href="#home">Home</Nav.Link>
-            <Nav.Link className='navLink' href="#link">React</Nav.Link>
-            <Nav.Link className='navLink' href="#link">Unity</Nav.Link>
-            <Nav.Link className='navLink' href="#link">3D Modeling</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <ul className='header'>
+        <li>Home</li>
+        <li>React</li>
+        <li>Unity</li>
+        <li>3D Modeling</li>
+        <li>Contact me</li>
+      </ul>
+    
   );
 }
 
