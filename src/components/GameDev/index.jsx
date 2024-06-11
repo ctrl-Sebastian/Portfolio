148683b86fca3a49e3acf4244ec111657a67b0c2
sebastian <sebastianipl0131@gmail.com>
2024-06-10 21:06:21 -0400

    inicio gamedev

--- src/components/GameDev/index.jsx
+++ src/components/GameDev/index.jsx
@@ -1,62 +1,34 @@
 import './GameDev.css'
 import React from 'react'
+import {Carousel, CarouselItem} from 'react-bootstrap'
 import { Card, CardBody, CardImg, CardText } from 'react-bootstrap'
-import bg1 from '../../imgs/bg1.jpg'
+import bg2 from '../../imgs/bg2.png'
+import bg3 from '../../imgs/bg3.png'
 
 function GameDevSection() {
   return (
     <section className='section-react'>
     <h1>Game Dev projects</h1>
-    <div className='react-project-list'>
-      <Card>
-        <CardImg src={bg1}></CardImg>
-        <CardBody>
-          <CardText>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-            Error commodi expedita voluptate ipsum ad repellat, quas quam sit temporibus esse perferendis reiciendis assumenda reprehenderit iure. 
-            Ipsa nemo quo assumenda illo.</CardText>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg src={bg1}></CardImg>
-        <CardBody>
-          <CardText>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-            Error commodi expedita voluptate ipsum ad repellat, quas quam sit temporibus esse perferendis reiciendis assumenda reprehenderit iure. 
-            Ipsa nemo quo assumenda illo.</CardText>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg src={bg1}></CardImg>
-        <CardBody>
-          <CardText>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-            Error commodi expedita voluptate ipsum ad repellat, quas quam sit temporibus esse perferendis reiciendis assumenda reprehenderit iure. 
-            Ipsa nemo quo assumenda illo.</CardText>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg src={bg1}></CardImg>
-        <CardBody>
-          <CardText>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-            Error commodi expedita voluptate ipsum ad repellat, quas quam sit temporibus esse perferendis reiciendis assumenda reprehenderit iure. 
-            Ipsa nemo quo assumenda illo.</CardText>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg src={bg1}></CardImg>
-        <CardBody>
-          <CardText>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-            Error commodi expedita voluptate ipsum ad repellat, quas quam sit temporibus esse perferendis reiciendis assumenda reprehenderit iure. 
-            Ipsa nemo quo assumenda illo.</CardText>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg src={bg1}></CardImg>
-        <CardBody>
-          <CardText>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-            Error commodi expedita voluptate ipsum ad repellat, quas quam sit temporibus esse perferendis reiciendis assumenda reprehenderit iure. 
-            Ipsa nemo quo assumenda illo.</CardText>
-        </CardBody>
-      </Card>
-    </div>
+    <Carousel fade className='carousel-gamedev'>
+      <Carousel.Item>
+        {/*<div className='carousel-item-info'>
+          <h1>Title</h1>
+          <section className='carousel-item-imgs'>
+            <img src={bg3}></img>
+            <img src={bg3}></img>
+            <img src={bg3}></img>
+          </section>
+        </div>*/}
+        <img src={bg2}></img>
+        <Carousel.Caption>
+          <section className='carousel-item-caption'>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </section>
+        </Carousel.Caption>
+        
+      </Carousel.Item>
+    </Carousel>
   </section>
   )
 }
